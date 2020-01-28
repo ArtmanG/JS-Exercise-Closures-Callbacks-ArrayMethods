0b0ca962b69d4d4382c6e95841e6d47a7f031ed6
Gregory Hawman <g.hawman@gmail.com>
2020-01-28 14:07:53 -0800

    finished challenge firstNamesAllCaps.

--- index.js
+++ index.js
@@ -238,8 +238,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let namesAllCaps = runners.map(function(item) {
+    return item.first_name.toUpperCase();
+  });
+  return namesAllCaps;
 }
 
 /**
